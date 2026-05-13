462ca0df27ad82fbb235648f4fe85cc2d1c4366a
zudydy <kjyjykwak@gmail.com>
2026-05-13 20:38:42 +0900

    fix: path_to_file_list

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    li = open(path, 'w')
+    lines = open(path, 'r').read().split('\n')
     return lines
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
